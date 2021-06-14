4e92ab9a1bcd560ca0ca6648d85e6037c7f503bd
kabuto91 <1034450752@qq.com>
2021-06-14 23:45:51 +0800

    改变了头部页面

--- blog/components/Header.js
+++ blog/components/Header.js
@@ -4,9 +4,10 @@ import Router from 'next/router';
 import Link from 'next/link';
 import axios from 'axios';
 import servicePath from '../config/apiUrl';
-
-import {Row,Col, Menu, Icon} from 'antd';
+import {Row,Col, Menu, Icon, Affix} from 'antd';
 import Item from 'antd/lib/list/Item';
+
+const {SubMenu}  = Menu;
 
 
 
@@ -30,29 +31,51 @@ const Header = () => {
             Router.push('/list?id='+e.key)
         }
     }
+    console.log(navArray)
 
     return (
-            <div className="header">
+           <Affix offsetTop="0">
+                <div className="header">
         <Row type="flex" justify="center">
-            <Col  xs={24} sm={24} md={10} lg={15} xl={12}>
-                <span className="header-logo">技术胖</span>
-                <span className="header-txt">专注前端开发,每年100集免费视频。</span>
+            <Col  xs={24} sm={24} md={10} lg={15} xl={8}>
+                <span className="header-logo">Kabuto91</span>
+                <span className="header-txt">苦海无涯，努力学习</span>
             </Col>
 
-            <Col className="memu-div" xs={0} sm={0} md={14} lg={8} xl={6}>
-                <Menu  mode="horizontal" onClick={handleClick}>
+            <Col className="memu-div" xs={0} sm={0} md={14} lg={12} xl={10}>
+                <Menu  mode="horizontal" onClick={handleClick} className="header-list">
                 <Menu.Item key="0">
-                                    <Icon type="home" />
+                                    <Icon type="home"  theme="twoTone"/>
                                     博客首页
                                 </Menu.Item>
                     {
                         navArray.map((item)=>{
-                            return (
-                                <Menu.Item key={item.id}>
-                                    <Icon type={item.icon} />
-                                    {item.typeName}
-                                </Menu.Item>
-                            )
+                            if(item.typePriority==1){
+                                if(item.hasChild==1){
+                                    return (
+                                        <SubMenu
+                                    title={
+                                        <span>
+                                            <Icon type={item.icon} theme="twoTone"  />
+                                            {item.typeName}
+                                        </span>
+                                    }>
+                                        <Menu.ItemGroup>
+                                            <Menu.Item key="4">宠物小精灵</Menu.Item>
+                                            <Menu.Item key="5">假面骑士</Menu.Item>
+                                        </Menu.ItemGroup>
+                                    </SubMenu>
+                                    )
+                                }else{
+                                    return (
+                                    <Menu.Item key={item.id}>
+                                        <Icon type={item.icon} theme="twoTone" />
+                                        {item.typeName}
+                                    </Menu.Item>
+                                )
+                                }
+                            }
+                            
                         })
                     }
                     
@@ -61,6 +84,7 @@ const Header = () => {
             </Col>
         </Row>
     </div>
+           </Affix>
     )
   
 }
