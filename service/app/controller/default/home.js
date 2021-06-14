2a25bf5716ecbec564826e9a3bbddfdb4759cb4d
kabuto91 <1034450752@qq.com>
2021-06-14 23:47:29 +0800

    增加了功能API

--- service/app/controller/default/home.js
+++ service/app/controller/default/home.js
@@ -14,10 +14,11 @@ class HomeController extends Controller {
       let sql = 'SELECT article.id as id,'+
       'article.title as title,'+
       'article.introduce as introduce,'+
-      "FROM_UNIXTIME(article.addTime, '%y-%m-%d %H:%i:%s') as addTime,"+
+      "FROM_UNIXTIME(article.addTime, '%y-%m-%d') as addTime,"+
       'article.view_count as view_count ,'+
-      '.type.typeName as typeName '+
-      'FROM article LEFT JOIN type ON article.type_id = type.Id';
+      '.typetable.typeName as typeName '+
+      'FROM article LEFT JOIN typetable ON article.type_id = typetable.Id ' +
+      'WHERE article.type_id=1';
 
       const results = await this.app.mysql.query(sql);
 
@@ -25,6 +26,70 @@ class HomeController extends Controller {
           data:results
       }
   }
+
+  async getArticleListLimit(){
+      
+
+    let sql = 'SELECT article.id as id,'+
+  'article.title as title,'+
+  "FROM_UNIXTIME(article.addTime, '%y-%m-%d') as addTime,"+
+  '.typetable.typeName as typeName '+
+  'FROM article LEFT JOIN typetable ON article.type_id = typetable.Id ' +
+  'WHERE article.type_id=1  ' +
+  'ORDER BY article.addTime DESC LIMIT 6';
+
+    const results = await this.app.mysql.query(sql);
+
+    this.ctx.body = {
+        data:results
+    }
+}
+
+async getComputerListLimit(){
+      
+
+  let sql = 'SELECT article.id as id,'+
+  'article.title as title,'+
+  "FROM_UNIXTIME(article.addTime, '%y-%m-%d') as addTime,"+
+  '.typetable.typeName as typeName '+
+  'FROM article LEFT JOIN typetable ON article.type_id = typetable.Id ' +
+  'WHERE article.type_id=2 ' +
+  'ORDER BY article.addTime DESC LIMIT 6';
+
+  const results = await this.app.mysql.query(sql);
+
+  this.ctx.body = {
+      data:results
+  }
+}
+
+async getFunListLimit(){
+      
+
+  let sql = 'SELECT article.id as id,'+
+  'article.title as title,'+
+  "FROM_UNIXTIME(article.addTime, '%y-%m-%d') as addTime,"+
+  '.typetable.typeName as typeName '+
+  'FROM article LEFT JOIN typetable ON article.type_id = typetable.Id ' +
+  'WHERE article.type_id=4  OR article.type_id=5 ' +
+  'ORDER BY article.addTime DESC LIMIT 6';
+
+  const results = await this.app.mysql.query(sql);
+
+  this.ctx.body = {
+      data:results
+  }
+}
+
+async getArticleCount(){
+  let sql = 'SELECT typetable.typeName, COUNT(*) as articleCount FROM article LEFT JOIN typetable ON article.type_id=typetable.id GROUP BY type_id';
+
+  const results = await this.app.mysql.query(sql);
+
+  this.ctx.body = {
+      counts:results
+  }
+}
 
   async getArticleById(){
     //先配置路由的动态传值，然后再接收值
@@ -34,11 +99,11 @@ class HomeController extends Controller {
         'article.title as title,'+
         'article.introduce as introduce,'+
         'article.article_content as article_content,'+
-        "FROM_UNIXTIME(article.addTime,'%Y-%m-%d %H:%i:%s' ) as addTime,"+
+        "FROM_UNIXTIME(article.addTime,'%Y-%m-%d' ) as addTime,"+
         'article.view_count as view_count ,'+
-        'type.typeName as typeName ,'+
-        'type.id as typeId '+
-        'FROM article LEFT JOIN type ON article.type_id = type.Id '+
+        'typetable.typeName as typeName ,'+
+        'typetable.id as typeId '+
+        'FROM article LEFT JOIN typetable ON article.type_id = typetable.Id '+
         'WHERE article.id='+id;
 
     const result = await this.app.mysql.query(sql);
@@ -50,7 +115,7 @@ class HomeController extends Controller {
 
   //得到类别名称和编号
   async getTypeInfo(){
-    const result = await this.app.mysql.select('type');
+    const result = await this.app.mysql.select('typetable');
     this.ctx.body = {data:result}
   }
 
@@ -60,10 +125,10 @@ class HomeController extends Controller {
     let sql = 'SELECT article.id as id,'+
     'article.title as title,'+
     'article.introduce as introduce,'+
-    "FROM_UNIXTIME(article.addTime,'%Y-%m-%d %H:%i:%s' ) as addTime,"+
+    "FROM_UNIXTIME(article.addTime,'%Y-%m-%d' ) as addTime,"+
     'article.view_count as view_count ,'+
-    'type.typeName as typeName '+
-    'FROM article LEFT JOIN type ON article.type_id = type.Id '+
+    'typetable.typeName as typeName '+
+    'FROM article LEFT JOIN typetable ON article.type_id = typetable.Id '+
     'WHERE type_id='+id
     const result = await this.app.mysql.query(sql)
     this.ctx.body={data:result}
