8b197af8089b3bb5c173fd0aa9602f1840659dac
kabuto91 <1034450752@qq.com>
2021-06-14 23:48:06 +0800

    增加了功能

--- service/app/router/default.js
+++ service/app/router/default.js
@@ -2,6 +2,11 @@ module.exports = app =>{
     const {router, controller} = app;
     router.get('/default/index',controller.default.home.index);
     router.get('/default/getArticleList',controller.default.home.getArticleList);
+    router.get('/default/getArticleListLimit',controller.default.home.getArticleListLimit);
+    router.get('/default/getComputerListLimit',controller.default.home.getComputerListLimit);
+    router.get('/default/getFunListLimit',controller.default.home.getFunListLimit);
+    router.get('/default/getArticleCount',controller.default.home.getArticleCount);
+    
     router.get('/default/getArticleById/:id',controller.default.home.getArticleById);
     router.get('/default/getTypeInfo',controller.default.home.getTypeInfo);
     router.get('/default/getListById/:id',controller.default.home.getListById);
